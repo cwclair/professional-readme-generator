2030c8b8673e761c8594982e057d681935f1e964
Christopher Clair <124112751+cwclair@users.noreply.github.com>
2023-05-26 15:12:46 -0500

    trying write function

---
+++ README-auto.md
@@ -0,0 +1,37 @@
+# README
+
+    ## Table of Contents
+    1. [Description](#description)
+    2. [Installation](#installation)
+    3. [Usage](#usage)
+    4. [License](#license)
+    5. [Contributing](#contributing)
+    6. [Tests](#tests)
+    7. [Questions](#questions)
+    
+    ## Description <a name="description"></a>
+    This project generates a professional README document.
+    
+    ## Installation <a name="installation"></a>
+
+    none
+    
+    ## Usage <a name="usage"></a>
+
+    To generate the document, run node index.js from the command line.
+    
+    ## License <a name="license"></a>
+
+    other
+    
+    ## Contributing <a name="contributing"></a>
+
+    Classroom lessons.
+    
+    ## Tests <a name="tests"></a>
+    
+    none
+
+    ## Questions <a name="questions"></a>
+    GitHub profile: https://github.com/undefined   
+    Email address: christopher.w.clair@gmail.com
--- index.js
+++ index.js
@@ -54,7 +54,46 @@ inquirer
       name: 'emailAddress',
     },
 ])
+.then(response => {
+    fs.writeFile('README-auto.md', `# ${response.projectName}
 
+    ## Table of Contents
+    1. [Description](#description)
+    2. [Installation](#installation)
+    3. [Usage](#usage)
+    4. [License](#license)
+    5. [Contributing](#contributing)
+    6. [Tests](#tests)
+    7. [Questions](#questions)
+    
+    ## Description <a name="description"></a>
+    ${response.description}
+    
+    ## Installation <a name="installation"></a>
+
+    ${response.installation}
+    
+    ## Usage <a name="usage"></a>
+
+    ${response.usage}
+    
+    ## License <a name="license"></a>
+
+    ${response.license}
+    
+    ## Contributing <a name="contributing"></a>
+
+    ${response.collaborators}
+    
+    ## Tests <a name="tests"></a>
+    
+    ${response.tests}
+
+    ## Questions <a name="questions"></a>
+    GitHub profile: https://github.com/${response.githubUserName}   
+    Email address: ${response.emailAddress}`, (err) =>
+    err ? console.error(err) : console.log('Commit logged!')
+  )});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
