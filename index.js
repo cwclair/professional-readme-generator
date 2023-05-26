ebcd0eb1a49798c91c227efc7130205b77e133fa
Christopher Clair <124112751+cwclair@users.noreply.github.com>
2023-05-26 14:26:05 -0500

    Questions created

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is the title of your project?', 'Enter a description of your project.', 'Installation instructions (if applicable):', 'Provide instructions and examples for use.', 'List any collaborators, third-party assets, or tutorials you used to create this project.', 'What license did you use for this project?', 'List any test instructions.', 'Enter your GitHub profile link.', 'Enter your email address.'];
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
